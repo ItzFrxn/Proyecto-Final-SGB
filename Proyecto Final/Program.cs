d1aae380f3675450eb34529b719183bb98c3cc36
Francisco Melchor <fmelchor_@hotmail.com>
2026-04-27 19:07:35 -0500

    Actualizacion

--- Proyecto Final/Program.cs
+++ Proyecto Final/Program.cs
@@ -10,7 +10,39 @@ namespace Proyecto_Final
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to UAT-Pay!");
+            Console.WriteLine("=======================");
+            Console.WriteLine("\tUATPay");
+            Console.WriteLine("=======================");
+            Console.WriteLine("Welcome to UAT-Pay!"); 
+            Console.WriteLine("1) Ingresar\n2) Registrarse");
+            int op = Convert.ToInt32(Console.ReadLine());
+            switch (op)
+            {
+                case 1:
+                    Console.WriteLine("=======================");
+                    Console.WriteLine("\tUATPay");
+                    Console.WriteLine("=======================");
+                    Console.WriteLine("Proporcione los siguientes datos.");
+                    Console.Write("Usuario: ");
+                    string user = Console.ReadLine();
+                    Console.Write("Contraseña: ");
+                    string pass = Console.ReadLine();
+                    break;
+                case 2:
+                    Console.WriteLine("=======================");
+                    Console.WriteLine("\tUATPay");
+                    Console.WriteLine("=======================");
+                    Console.WriteLine("Proporcione los siguientes datos.");
+                    Console.Write("Nombre: ");
+                    string rName = Console.ReadLine();
+                    Console.Write("Apellidos: ");
+                    string rLastN = Console.ReadLine();
+                    Console.Write("Edad: ");
+                    int rAge = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("Tipo de cuenta (chequera / credito / inversion ): ");
+                    string rType = Console.ReadLine();
+                    break;
+            }
         }
     }
 }
--- Proyecto Final/cuenta.cs
+++ Proyecto Final/cuenta.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -17,22 +18,74 @@ namespace Proyecto_Final
         private double saldo;
         private string tipo;
 
+        // Propiedades
+        public int Registro
+        {
+            get { return this.numRegistro; }
+            set { this.numRegistro = value; }
+        }
+        public string Nombre
+        {
+            get { return this.nombres; }
+            set { this.nombres = value; }
+        }
+        public string Apellido
+        {
+            get { return this.apellidos; }
+            set { this.apellidos = value; }
+        }
+        public int Edad
+        {
+            get { return this.edad; }
+            set { this.edad = value; }
+        }
+        public string Apertura
+        {
+            get { return this.fechaApt; }
+            set { this.fechaApt = value; }
+        }
+        public double Saldo
+        {
+            get { return this.saldo; }
+            set { this.saldo = value; }
+        }
+        public string Tipo
+        {
+            get { return this.tipo; } 
+            set { this.tipo = value; }
+        }
+
+        // Constructores
+        public Cuenta(string nombre, string apellido, int edad, string tipo)
+        {
+
+        }
 
     }
     // CLASE  SECUNDARIAS - CHEQUERA
     public class Chequera : Cuenta
     {
-
+        public Chequera(string nombre, string apellido, int edad) : base(nombre, apellido, edad, "Chequera")
+        {
+        }
     }
     // CLASE - CREDITO
     public class Credito : Cuenta
     {
         private int numTarjeta;
+
+        public Credito(string nombre, string apellido, int edad) : base(nombre, apellido, edad, "Credito")
+        {
+        }
     }
     // CLASE - INVERSION
     public class Inversion : Cuenta
     {
         private double tasa;
         private string periodo;
+
+        public Inversion(string nombre, string apellido, int edad) : base(nombre, apellido, edad, "Inversion")
+        {
+        }
     }
 }
